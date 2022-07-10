ad63a7a3fd6ea4edda6181c1a67c676fee82e5e2
Екатерина Калистратова <kalistratova_k@mail.ru>
2022-07-10 12:06:39 +0300

    clean error

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -62,19 +62,5 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForUnRegisteredAndUnderLimitF() {
-        BonusService service = new BonusService();
 
-        // подготавливаем данные:
-        long amount = 1000;
-        boolean registered = false;
-        long expected = 20;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
 }
